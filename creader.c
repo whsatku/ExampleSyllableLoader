dec944ddd207cf6a945df63e689b4fff1f940601
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-02-11 08:51:54 +0700

    Added utf8 conversion for creader

--- creader.c
+++ creader.c
@@ -1,6 +1,9 @@
+// Compile this file:
+// gcc -liconv -o creader creader.c
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <iconv.h>
 
 #define c_time int64_t
 
@@ -30,7 +33,7 @@ void syllable_skip_to_data(FILE* fp){
 	fseek(fp, 256, SEEK_SET);
 }
 
-int syllable_read_record(FILE* fp, DATA_RECORD* record, int timeSize){
+int syllable_read_record(FILE* fp, iconv_t charset, DATA_RECORD* record, int timeSize){
 	if(fread(&record->header, sizeof(record->header), 1, fp) != 1){
 		return 0;
 	}
@@ -65,16 +68,20 @@ int syllable_read_record(FILE* fp, DATA_RECORD* record, int timeSize){
 		record->timestamp = (int64_t) timestamp;
 	}
 
-	int i;
-	for(i = 0; i < sizeof(record->szText); i++){
-		record->szText[i] = fgetc(fp);
+	char buffer[1023];
+	for(int i = 0; i < sizeof(buffer); i++){
+		buffer[i] = fgetc(fp);
 
-		if(record->szText[i] == 0){
-			i++;
+		if(buffer[i] == 0){
 			break;
 		}
 	}
-	record->szText[i] = 0;
+	size_t inLeft = 1023;
+	size_t outLeft = 1023;
+	char *input = &buffer[0];
+	char *output = &record->szText[0];
+	iconv(charset, &input, &inLeft, &output, &outLeft);
+	record->szText[1023] = 0;
 
 	return 1;
 }
@@ -88,10 +95,11 @@ int main(int argc, char *argv[]){
 	syllable_skip_to_data(fp);
 
 	DATA_RECORD* record = (DATA_RECORD*) malloc(sizeof(DATA_RECORD));
+	iconv_t charset = iconv_open("UTF8", "CP874");
 	int lastId = 0;
 	int timeSize = TIME_DETECT;
 	while(!feof(fp)){
-		if(syllable_read_record(fp, record, timeSize) != 1){
+		if(syllable_read_record(fp, charset, record, timeSize) != 1){
 			break;
 		}
 
